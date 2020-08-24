059a76c1f626307d93fe1dad564a8c660c273520
Ball <piccomed@gmail.com>
2020-08-24 22:38:40 +0700

    Update campaign.js

--- ethereum/campaign.js
+++ ethereum/campaign.js
@@ -2,5 +2,5 @@ import web3 from './web3';
 import Campaign from './build/Campaign.json';
 
 export default address => {
-  return new web3.eth.Contract(JSON.parse(Campaign.interface), address);
+  return new web3.eth.Contract(JSON.parse(Campaign.interface), '0xCA7740C40E82f945D4e48b9Cf2475c2674B2813D');
 };
